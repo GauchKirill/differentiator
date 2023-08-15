a814b52d4ec5761bc42d851d36fb063324534400
Gauch Kirill <kgauch@mail.ru>
2023-08-15 19:04:42 +0300

    начал исправлять calculate.cpp

--- modes/calculate.cpp
+++ modes/calculate.cpp
@@ -1,17 +1,30 @@
-#include "calculate.h"
+#include "./calculate.h"
 
 bool calculate_node(node* now_node)
 {
 	if (!now_node)
 		return false;
+
 	bool did_subtrees_calculate = false;
-	if(now_node->type == OPERATION)
+	if (type(now_node) == OPERATION)
 	{
 		did_subtrees_calculate = (calculate_node(left_node(now_node)) | calculate_node(right_node(now_node)));
 		did_subtrees_calculate |= is_calculate(now_node);
 	}
 	return did_subtrees_calculate;
 }
+
+#define DEF_OP(name, priority, operator)																	\
+	case OP_ ## name:																						\
+		if (type(right_node(now_node)) == NUMBER &&															\
+			type(left_node(now_node))  == NUMBER)															\
+		{																									\
+			type(now_node) = NUMBER;																		\
+			value_num(now_node) = value_num(left_node(now_node)) operator value_num(right_node(now_node));	\
+		}																									\
+		break;
+
+#define DEF_FUNC(name, priority, operator)
 
 #define calculate_simple_op(name, op)												\
 	case OP_ ## name:																\
@@ -42,35 +55,22 @@ bool calculate_node(node* now_node)
 
 bool is_calculate(node* now_node)
 {
+	if (!now_node || type(now_node) != OPERATION) return false;
+
 	switch(num_op(now_node))
 	{
-		calculate_func(SIN, sin)
-		calculate_func(COS, cos)
-		calculate_func(LOG, log)
-		calculate_func(TG,  tan)
-		calculate_simple_op(ADD, +)
-		calculate_simple_op(SUB, -)
-		calculate_simple_op(MUL, *)
-		calculate_simple_op(DIV, /)
-		case OP_POW:
-			if (right_node(now_node)->type == NUMBER &&
-				left_node(now_node)->type == NUMBER)
-			{
-				now_node->type = NUMBER;
-				value_num(now_node) = pow(value_num(left_node(now_node)), value_num(right_node(now_node)));	
-				node_dtor(left_node(now_node));												
-				node_dtor(right_node(now_node));												
-				left_node(now_node)  = nullptr;												
-				right_node(now_node) = nullptr;	
-				return true;											
-			}
-			else
-				return false;
-			break;
-		default:
-			printf("Cann't calculate operator of number: %d\n", num_op(now_node));
-			break;
+		#include "../settings/operations.h"
+
 	}
+
+	if (type(now_node) == NUMBER)
+	{
+		node_dtor(left_node(now_node));
+		node_dtor(right_node(now_node));
+		left_node(now_node) = nullptr;
+		right_node(now_node) = nullptr;
+	}
+	
 	return false;
 }
 
--- modes/calculate.h
+++ modes/calculate.h
@@ -1,7 +1,7 @@
 #ifndef CALCULATE_H
 #define CALCULATE_H
 
-#include "tree.h"
+#include "../struct/tree.h"
 
 bool calculate_node(node* now_node);
 bool is_calculate(node* now_node);
